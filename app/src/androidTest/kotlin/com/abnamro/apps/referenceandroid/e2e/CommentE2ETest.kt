c70db7d69730d21e21bb775fb07a59e022409260
Aldo Christian <aldochristianpareira@gmail.com>
2025-07-20 16:01:29 +0200

    Revert test for orientation

--- app/src/androidTest/kotlin/com/abnamro/apps/referenceandroid/e2e/CommentE2ETest.kt
+++ app/src/androidTest/kotlin/com/abnamro/apps/referenceandroid/e2e/CommentE2ETest.kt
@@ -6,7 +6,12 @@ import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.test.espresso.device.DeviceInteraction.Companion.setScreenOrientation
+import androidx.test.espresso.device.EspressoDevice.Companion.onDevice
+import androidx.test.espresso.device.action.ScreenOrientation
+import androidx.test.espresso.device.rules.ScreenOrientationRule
 import com.abnamro.apps.referenceandroid.MainActivity
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 
@@ -15,8 +20,23 @@ class CommentE2ETest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule(MainActivity::class.java)
 
+    @get:Rule
+    val screenOrientationRule: ScreenOrientationRule = ScreenOrientationRule(ScreenOrientation.PORTRAIT)
+
     @Test
     fun validateCommentAreLoadedAndSnackBarDisplayed() {
+        composeTestRule.onNodeWithTag("reload_button").performClick()
+        composeTestRule.waitUntil(timeoutMillis = 5_000) {
+            composeTestRule.onAllNodesWithText("Comments loaded!").fetchSemanticsNodes()
+                .isNotEmpty()
+        }
+        composeTestRule.onNodeWithText("Comments loaded!").assertIsDisplayed()
+    }
+
+    @Test
+    @Ignore("Not working on Emulator")
+    fun validateCommentAreLoadedAndSnackBarDisplayedRotatedDevice() {
+        onDevice().setScreenOrientation(ScreenOrientation.LANDSCAPE)
         composeTestRule.onNodeWithTag("reload_button").performClick()
         composeTestRule.waitUntil(timeoutMillis = 5_000) {
             composeTestRule.onAllNodesWithText("Comments loaded!").fetchSemanticsNodes()
